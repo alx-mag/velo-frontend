6ee9c086ffc184479417b617f53cabf1777090d4
Your Name <darya_leushkina@mail.ru>
2019-07-21 14:21:13 +0400

    done first page

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@
 
                         <b-row>
                             <b-col>
-                                <b-button class="sendButton">
+                                <b-button class="sendButton" @click="showModal">
                                     Отправить
                                 </b-button>
                             </b-col>
@@ -74,7 +74,36 @@
                                         <img src="./assets/map.svg">
                                     </div>
                                     <div class="bottomMap">
+                                        <b-row>
+                                            <b-col cols="2">
+                                                <img src="./assets/subwayLine.svg" class="subway">
+                                            </b-col>
+                                            <b-col cols="4">
+                                                <div class="placePoint">
+                                                    <b-row class="rowFromPoint">
+                                                        <b-col >
+                                                            Площадь Ленина
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row>
+                                                        <b-col class="rowToPoint">
+                                                            Детский парк
+                                                        </b-col>
+                                                    </b-row>
+                                                </div>
 
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img class="likeImg" v-on:click="likeIt();" src="./assets/like.svg">
+                                                </b-button>
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img src="./assets/comment.svg" class="commentImg">
+                                                </b-button>
+                                            </b-col>
+                                        </b-row>
                                     </div>
                                 </div>
                             </b-col>
@@ -84,41 +113,141 @@
                                         <img src="./assets/map.svg">
                                     </div>
                                     <div class="bottomMap">
+                                        <b-row>
+                                            <b-col cols="2">
+                                                <img src="./assets/subwayLine.svg" class="subway">
+                                            </b-col>
+                                            <b-col cols="4">
+                                                <div class="placePoint">
+                                                    <b-row class="rowFromPoint">
+                                                        <b-col >
+                                                            Площадь Ленина
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row>
+                                                        <b-col class="rowToPoint">
+                                                            Детский парк
+                                                        </b-col>
+                                                    </b-row>
+                                                </div>
 
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img class="likeImg" v-on:click="likeIt();" src="./assets/noLike.svg">
+                                                </b-button>
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img src="./assets/comment.svg" class="commentImg">
+                                                </b-button>
+                                            </b-col>
+                                        </b-row>
                                     </div>
                                 </div>
                             </b-col>
                         </b-row>
-
                         <b-row>
                             <b-col cols="6">
-                                <div class="map">
+                                <div class="mapDown">
                                     <div class="topMap">
-                                        <img src="./assets/map.svg" width="100%" height="100%">
+                                        <img src="./assets/map.svg">
                                     </div>
                                     <div class="bottomMap">
+                                        <b-row>
+                                            <b-col cols="2">
+                                                <img src="./assets/subwayLine.svg" class="subway">
+                                            </b-col>
+                                            <b-col cols="4">
+                                                <div class="placePoint">
+                                                    <b-row class="rowFromPoint">
+                                                        <b-col >
+                                                            Площадь Ленина
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row>
+                                                        <b-col class="rowToPoint">
+                                                            Детский парк
+                                                        </b-col>
+                                                    </b-row>
+                                                </div>
 
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img class="likeImg" v-on:click="likeIt();" src="./assets/like.svg">
+                                                </b-button>
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img src="./assets/comment.svg" class="commentImg">
+                                                </b-button>
+                                            </b-col>
+                                        </b-row>
                                     </div>
                                 </div>
                             </b-col>
                             <b-col cols="6">
-                                <div class="map">
+                                <div class="mapDown">
                                     <div class="topMap">
                                         <img src="./assets/map.svg" >
                                     </div>
                                     <div class="bottomMap">
+                                        <b-row>
+                                            <b-col cols="2">
+                                                <img src="./assets/subwayLine.svg" class="subway">
+                                            </b-col>
+                                            <b-col cols="4">
+                                                <div class="placePoint">
+                                                    <b-row class="rowFromPoint">
+                                                        <b-col >
+                                                            Площадь Ленина
+                                                        </b-col>
+                                                    </b-row>
+                                                    <b-row>
+                                                        <b-col class="rowToPoint">
+                                                            Детский парк
+                                                        </b-col>
+                                                    </b-row>
+                                                </div>
 
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img class="likeImg" v-on:click="likeIt();" src="./assets/like.svg">
+                                                </b-button>
+                                            </b-col>
+                                            <b-col cols="3">
+                                                <b-button class="route">
+                                                    <img src="./assets/comment.svg" class="commentImg">
+                                                </b-button>
+                                            </b-col>
+                                        </b-row>
                                     </div>
                                 </div>
                             </b-col>
                         </b-row>
                     </div>
-
-
-
                 </b-col>
 
             </b-row>
+
+            <b-modal id="vote" hide-footer hide-header ref="vote" >
+                <div class="voteWindow">
+                    <div class="titleVote">
+                        СПАСИБО ЗА ПРЕДЛОЖЕННЫЙ МАРШРУТ!
+                    </div>
+                    <b-button class="newRoute sendButton"  @click="hideModal">
+                        Добавить новый
+                    </b-button>
+                    <div class="subTitleVote">
+                        Вы можете посмотреть предложения всех пользователей
+                    </div>
+                    <b-button class="openMap" @click="hideModal">
+                        Открыть карту
+                    </b-button>
+                </div>
+            </b-modal>
         </b-container fluid>
     </div>
 </template>
@@ -130,13 +259,29 @@
         name: 'app',
         components: {
             Map
-
         },
 
         data: function () {
             return {
-                coords: [51.534907, 46.035188]
+                coords: [51.534907, 46.035188],
+                like: false,
+                likePath: './assets/like.svg'
             };
+
+        },
+        methods: {
+            likeIt()
+            {
+                this.like = (this.likePath == './assets/like.svg' ? ',/assets/noLike.svg' : './assets/like.svg');
+            },
+            showModal() {
+                this.$refs['vote'].show()
+            },
+            hideModal() {
+                this.$refs['vote'].hide()
+                let input = $(div).find("input[type='file']");
+                input.replaceWith(input.val('').clone(true));
+            },
         }
     }
 </script>
@@ -152,7 +297,6 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
     }
 
     #app {
@@ -263,8 +407,6 @@
         color: #FFFFFF;
     }
 
-
-
     .inputPoint {
         width: 330px;
         height: 50px;
@@ -334,11 +476,19 @@
         height: 335px;
         border-radius: 6px;
         margin-left: 60px;
-        margin-top: 100px;
+        margin-top: 60px;
+        margin-right: 14px;
+        margin-bottom: 80px;
+    }
+    .mapDown {
+        width: 420px;
+        height: 335px;
+        border-radius: 6px;
+        margin-left: 60px;
+        margin-top: 20px;
         margin-right: 14px;
         margin-bottom: 100px;
     }
-
     .topMap {
         width: 420px;
         height: 262px;
@@ -353,7 +503,148 @@
     .block {
         margin-left: 60px;
     }
+    .subway {
+        margin-top: 27px;
+        margin-left: 33px;
 
+    }
+    .placePoint {
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 12px;
+        line-height: 15px;
+
+        color: #9381FF;
+        text-align: left;
+    }
+    .rowFromPoint {
+        margin-top: 20px;
+        margin-left: 11px;
+        margin-right: 88px;
+    }
+    .rowToPoint {
+        margin-top: 5px;
+    }
+    .likeImg {
+        margin-top: 20px;
+        margin-right: 24px;
+    }
+    .commentImg {
+        margin-top: 20px;
+        margin-right: 36px;
+    }
+    .route, .route.btn, .route.btn-secondary:not(:disabled):not(.disabled).active, .route.btn-secondary:not(:disabled):not(.disabled):active, .route.show>.btn-secondary.dropdown-toggle, .route.btn:hover,.route.btn:active, .route.btn:focus {
+        box-shadow: none;
+        outline: none;
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+        text-transform: none;
+        background: #FFFFFF;
+        border: 0px;
+        padding: 0px;
+
+    }
+
+    button:focus, div:focus {
+        box-shadow: none !important;
+        border: none !important;
+        outline: none !important;
+    }
+
+    .voteWindow {
+        width: 420px;
+        height: 348px;
+
+
+    }
+    .titleVote {
+        width: 303px;
+        height: 81px;
+
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 22px;
+        line-height: 27px;
+        text-align: center;
+        letter-spacing: 0.05em;
+
+        color: #9381FF;
+
+        margin-left: 79px;
+        margin-right: 58px;
+        margin-top: 43px;
+
+
+    }
+    .subTitleVote {
+        width: 335px;
+        height: 44px;
+
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 22px;
+        text-align: center;
+
+        color: #9381FF;
+        margin-top: 48px;
+        margin-left: 53px;
+        margin-right: 42px;
+    }
+    .newRoute, .newRoute.btn, .newRoute.btn-secondary:not(:disabled):not(.disabled).active, .newRoute.btn-secondary:not(:disabled):not(.disabled):active, .newRoute.show>.btn-secondary.dropdown-toggle, .newRoute.btn:hover,.newRoute.btn:active, .newRoute.btn:focus {
+        width: 239px;
+        height: 43px;
+        margin-left: 111px;
+        margin-top: 30px;
+        margin-right: 90px;
+
+        background: #9381FF;
+        border-radius: 35px;
+        border-color: #9381FF;
+        box-shadow: none;
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 17px;
+        /* identical to box height */
+
+        text-align: center;
+        cursor: pointer;
+
+        color: #FFFFFF;
+
+    }
+    .openMap, .openMap.btn, .openMap.btn-secondary:not(:disabled):not(.disabled).active, .openMap.btn-secondary:not(:disabled):not(.disabled):active, .openMap.show>.btn-secondary.dropdown-toggle, .openMap.btn:hover,.openMap.btn:active, .openMap.btn:focus {
+        width: 239px;
+        height: 43px;
+
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 17px;
+        /* identical to box height */
+
+        text-align: center;
+
+        color: #9381FF;
+        border: 2px solid #9381FF;
+        box-sizing: border-box;
+        border-radius: 35px;
+
+        cursor: pointer;
+        background-color: #FFFFFF;
+
+        margin-left: 111px;
+        margin-top: 30px;
+        margin-right: 90px;
+
+    }
     @import url(http://fonts.googleapis.com/css?family=Montserrat:700,300,600,800,400&subset=cyrillic,cyrillic-ext,latin);
 
 
